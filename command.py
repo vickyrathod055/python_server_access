ed809b9626a93ada5ff0d088b3264aef7c97d390
vickyrathod055 <vicky.rathod@wipro.com>
2017-07-18 15:19:20 +0530

    Update command.py

--- command.py
+++ command.py
@@ -1,4 +1,5 @@
 ######################### class for executing commands ######################
+import paramiko
 class commands:
     def __init__(self,fqdn,u_name,u_pw):  #### here fqdn is fully qualified domain name
      self.ssh = paramiko.SSHClient()
@@ -14,6 +15,7 @@ class commands:
 	out = stdout.readlines()
 	for line in out:
           print line
+########creating new user ############
     def createUser(self,u_name,u_pw):
         stdin,stdout,stderr=self.ssh.exec_command("adduser "+u_name)
         stdin.write(u_pw)
@@ -22,11 +24,13 @@ class commands:
         out=stdout.readlines()
         for line in out:
           print line
+######## deleting users ########
     def deleteUser(self,u_name):
         stdin,stdout,stderr=self.ssh.exec_command("deluser --remove-home "+u_name)
         out=stdout.readlines()
         for line in out:
           print line
+############ deleting groups #########
     def deleteGroup(self,g_name):
         stdin,stdout,stderr=self.ssh.exec_command("groupdel "+g_name)
         out=stdout.readlines()
