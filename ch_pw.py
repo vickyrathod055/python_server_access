da6d8c14a4f3ffe1fa51ccc1a555f72e477d33f9
vickyrathod055 <vickyrathod055@instance-3.c.regal-module-166212.internal>
2017-07-18 08:34:15 +0000

    7/18/2017

--- ch_pw.py
+++ ch_pw.py
@@ -3,15 +3,44 @@ import csv
 from con import s_connection
 from csvpath import path
 from command import commands
-f_path=path("ch_pw")  ############## finding path of password change csv
-with open(f_path.getPath()) as csvfile:     
-	readCSV = csv.DictReader(csvfile)     ################## changing rows of csv in dictionary
-	for row in readCSV:
-	  	u_name=row["Username"]
-		u_pw=row["Password"] 
-		n_pw=row["NewPassword"]	
-          	fqdn=row["Hostname"]+"."+row["domain"]+".com"
-		commands.changePass(fqdn,u_name,u_pw,n_pw) ####### change password command execution 
-		
-	   	
-	  
+chpw_path=path("ch_pw")  ############## finding path of password change cs
+createuser_path=path("create_user")
+deleteuser_path=path("delete_user")
+deletegroup_path=path("delete_group")
+with open(chpw_path.getPath()) as csvfile:     
+    readCSV = csv.DictReader(csvfile)     ################## changing rows of csv in dictionary
+    for row in readCSV:
+	u_name=row["Username"]
+	u_pw=row["Password"] 
+	n_pw=row["NewPassword"]	
+        fqdn=row["Hostname"]+"."+row["domain"]+".com"
+	obj1=commands(fqdn,u_name,u_pw) ####### change password command execution 
+	obj1.changePassword(n_pw,u_pw)
+with open(createuser_path.getPath()) as csvfile:
+    readCSV=csv.DictReader(csvfile)
+    for row in readCSV:
+        u_name=row["user_Username"]
+        u_pw=row["user_Password"]
+        administrator=row["Admin_Username"]
+        administrator_pw=row["Admin_Password"]
+        fqdn=row["Hostname"]+"."+row["domain"]+".com"
+        obj1=commands(fqdn,administrator,administrator_pw)
+        obj1.createUser(u_name,u_pw)
+with open(deleteuser_path.getPath()) as csvfile:
+    readCSV=csv.DictReader(csvfile)
+    for row in readCSV:
+        u_name=row["user_Username"]
+        administrator=row["Admin_Username"]
+        administrator_pw=row["Admin_Password"]
+        fqdn=row["Hostname"]+"."+row["domain"]+".com"
+        obj1=commands(fqdn,administrator,administrator_pw)
+        obj1.deleteUser(u_name)
+with open(deletegroup_path.getPath()) as csvfile:
+    readCSV=csv.DictReader(csvfile)
+    for row in readCSV:
+        g_name=row["Groupname"]
+        administrator=row["Admin_Username"]
+        administrator_pw=row["Admin_Password"]
+        fqdn=row["Hostname"]+"."+row["domain"]+".com"
+        obj1=commands(fqdn,administrator,administrator_pw)
+        obj1.deleteGroup(g_name)
--- command.py
+++ command.py
@@ -1,13 +1,34 @@
 ######################### class for executing commands ######################
 class commands:
- def changePass(fqdn,u_name,u_pw,n_pw):
-	c=s_connection(u_name,u_pw,fqdn)
-	ssh=c.getCon()
-	stdin, stdout, stderr =ssh.exec_command("passwd")
+    def __init__(self,fqdn,u_name,u_pw):  #### here fqdn is fully qualified domain name
+     self.ssh = paramiko.SSHClient()
+     self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+     self.ssh.connect(fqdn,username=u_name,password=u_pw)
+    ###      changing password             ########
+    def changePass(self,n_pw,u_pw):
+	stdin, stdout, stderr =self.ssh.exec_command("passwd")
 	stdin.write(u_pw+'\n')
 	stdin.write(n_pw+'\n')
 	stdin.write(n_pw+'\n')
 	stdin.flush()
 	out = stdout.readlines()
 	for line in out:
+          print line
+    def createUser(self,u_name,u_pw):
+        stdin,stdout,stderr=self.ssh.exec_command("adduser "+u_name)
+        stdin.write(u_pw)
+        stdin.write(u_pw)
+        stdin.flush()
+        out=stdout.readlines()
+        for line in out:
+          print line
+    def deleteUser(self,u_name):
+        stdin,stdout,stderr=self.ssh.exec_command("deluser --remove-home "+u_name)
+        out=stdout.readlines()
+        for line in out:
+          print line
+    def deleteGroup(self,g_name):
+        stdin,stdout,stderr=self.ssh.exec_command("groupdel "+g_name)
+        out=stdout.readlines()
+        for line in out:
           print line
